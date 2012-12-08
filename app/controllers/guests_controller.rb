a8f3f9a4dde207ac5cb4ff45fff5ba78481671be
ankit gupta <ankit.gupta8898@gmail.com>
2012-12-08 19:32:25 +0530

    couple of changes in the background

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,7 @@
 class GuestsController < ApplicationController
   # GET /guests
   # GET /guests.json
-   http_basic_authenticate_with :name => AUTH_CONFIG['credentials']['username'] ,:password => AUTH_CONFIG['credentials']['password'], :except => [:index, :new]
+   http_basic_authenticate_with :name => AUTH_CONFIG['credentials']['username'] ,:password => AUTH_CONFIG['credentials']['password'], :except => [:index, :new,:create]
   def index
     @guests = Guest.all
 
