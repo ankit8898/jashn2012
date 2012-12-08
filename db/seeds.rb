199e97b22693c9ca1c39c7428a0729a5a2cb94d0
ankit gupta <ankit.gupta8898@gmail.com>
2012-12-08 21:17:02 +0530

    added a seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+attendees = {male: ['Ankit Gupta','Ashish Upadhyae', 'Manohar'], female: ['Ekta Verma', 'Sneha Sharma', 'Yashasree']}
+
+attendees.each_pair do |key,value|
+	value.each do |name|
+      Guest.create! ({name: name,
+                      sex: key,
+                      attending: true})
+    end  
+end
