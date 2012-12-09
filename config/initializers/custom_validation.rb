f0e519313a74d6381082f5efee68d315945b70ba
ankit gupta <ankit.gupta8898@gmail.com>
2012-12-09 17:01:15 +0530

    some more validations

--- config/initializers/custom_validation.rb
+++ config/initializers/custom_validation.rb
@@ -1,12 +1,14 @@
  
 class JashnFunkyMessageValidator < ActiveModel::Validator
   def validate(record)
-    if record.name.blank? && record.sex.blank? && record.cell_num.blank?
-      record.errors[:base] << "We cannot move ahead without your Name, Sex and Mobile No."
+    if record.name.blank? && record.sex.blank? && record.cell_num.blank? && record.email_id.blank?
+      record.errors[:base] << "We cannot move ahead without your Name, Sex , Email and Mobile No."
      elsif record.name.blank?
       record.errors[:base] << "We Need your Name ."
      elsif record.sex.blank?
       record.errors[:base] << "How can you forget your Sex !!!"
+     elseif record.email_id.blank?
+       record.errors[:base] << "We want to send you a Invite,  Please give your Email."
      elsif Guest.pluck(:cell_num).include?(record.cell_num)
       record.errors[:base] << "We already have a entry in our Guest list with the number #{record.cell_num} ."	   
      else
